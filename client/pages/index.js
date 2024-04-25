312160a592e681a180dddf710b72d7c14597b26e
James Crovo <jamescrovo450@gmail.com>
2024-04-25 03:07:36 -0500

    add umami tracking link

--- client/pages/index.js
+++ client/pages/index.js
@@ -3,13 +3,15 @@ import styles from '../styles/home.module.scss';
 import { FaGithubAlt } from "react-icons/fa";
 import { TbShovel } from "react-icons/tb";
 import Link from 'next/link'; 
+import Script from 'next/script'
 
 export default function Home() {
   return (
     <div>
       <Head>
         <title>Shovel</title>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/favicon.ico" />                
+        <Script defer src="https://umami.ovel.sh/script.js" data-website-id="e4ae69a4-87b0-4bc9-acc6-abd1e533316f"></Script>
       </Head>
 
       <div className={styles.homeOuter}>
