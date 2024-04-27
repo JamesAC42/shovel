7ff3a544c126afc7cfd1e7713467d47b2929a42f
James Crovo <jamescrovo450@gmail.com>
2024-04-27 00:03:09 -0500

    Add screenshots to landing page

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,8 +1,9 @@
 import Head from 'next/head';
 import styles from '../styles/home.module.scss';
 import { FaGithubAlt } from "react-icons/fa";
-import { TbShovel } from "react-icons/tb";
+import { FaCircleArrowRight } from "react-icons/fa6";
 import Link from 'next/link'; 
+import Image from 'next/Image';
 
 export default function Home() {
   return (
@@ -21,9 +22,26 @@ export default function Home() {
             href="https://github.com/JamesAC42/shovel">
             <FaGithubAlt />
           </Link>
-          <Link href="/room">
-            <TbShovel />
+        </div>
+        <div className={styles.subheader}>
+          A Productivity Tool for Going Deep
+        </div>
+        <div className={styles.enter}>
+          <Link 
+            target="_self"
+            href="/room">
+            <span>
+              Try Now for Free 
+              <FaCircleArrowRight />
+            </span>
           </Link>
+        </div>
+
+        <div className={styles.screenshots}>
+          <Image src="/images/screenshots.gif"
+            width="100"
+            height="50"
+            alt="Screenshots"/>
         </div>
 
       </div>
