e4a7ea5886bad31edbf5a9844a237b7bbe5c360a
James Crovo <jamescrovo450@gmail.com>
2024-05-09 02:24:37 -0500

    update room table definition

--- sql/create-tables.sql
+++ sql/create-tables.sql
@@ -14,6 +14,7 @@ CREATE TABLE room
 (
     id integer NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 1 MINVALUE 1 MAXVALUE 2147483647 CACHE 1 ),
     name character varying(50) COLLATE pg_catalog."default",
+    public boolean,
     CONSTRAINT room_pkey PRIMARY KEY (id)
 );
 
